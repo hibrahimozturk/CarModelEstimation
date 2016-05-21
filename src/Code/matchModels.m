eaa35727e97259fdc4003d1bd6bff3cf6b2926a6
Halil İbrahim Öztürk <abc@muea.cmo>
2016-05-21 07:55:34 +0300

    load centers in matchModels.m

--- src/Code/matchModels.m
+++ src/Code/matchModels.m
@@ -5,13 +5,15 @@
 % numberOfFrames = 2100;
 % 
 % frames = read(v, [1 numberOfFrames]);
+
+%load('../data/matfiles/031805_0815_0915-objects-part1.mat');
+
 %%
 % grayFrames = zeros(size(frames,1)*4/10, size(frames,2)*4/10, 10, 'uint8');
 % 
 % for i=1:10
 %     grayFrames(:, :, i) = rgb2gray(imresize(frames(:, :, :, i), 0.4));
 % end
-
 
 %%
 matches = matchBoxes(grayFrames(:, :, :), centers(1:10), models);
